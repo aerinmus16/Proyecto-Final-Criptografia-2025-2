2a01edb40578fc4fd86c1e06df420923f977f203
lalo_gut1errez <lalogutierrez899@gmail.com>
2025-05-30 10:59:36 -0600

    módulos necesarios

---
+++ dispositivo.py
@@ -0,0 +1,9 @@
+import socket
+import pickle
+import signal
+import sys
+import os
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+
